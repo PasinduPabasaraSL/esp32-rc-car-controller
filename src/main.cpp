442006c32381256bde69160274f393299aeedbcc
Pasindu Pabasara <pabasarapasindu365@gmail.com>
2025-11-01 11:47:02 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+#define VRY_MOVE_PIN 34
+#define VRX_MOVE_PIN 32
+
+#define FORWARD_THRESHOLD  2800
+#define BACKWARD_THRESHOLD 3100
+
+#define LEFT_THRESHOLD  2800
+#define RIGHT_THRESHOLD 3100
+
+
+int valueMoveY = 0;
+int valueMoveX = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  valueMoveY = analogRead(VRY_MOVE_PIN);
+  valueMoveX = analogRead(VRX_MOVE_PIN);
+
+  if (valueMoveY < FORWARD_THRESHOLD) {
+    Serial.println("FORWARD");
+  }
+  else if (valueMoveY > BACKWARD_THRESHOLD) {
+    Serial.println("BACKWARD");
+  }
+  
+  if (valueMoveX < LEFT_THRESHOLD) {
+    Serial.println("LEFT");
+  }
+  else if (valueMoveX > RIGHT_THRESHOLD) {
+    Serial.println("RIGHT");
+  }
+
+  delay(200);
+}
